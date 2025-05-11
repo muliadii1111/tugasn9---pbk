aebe094b658ab8a7fd82cb2670fc74676dbc733e
muliadii1111 <muliadi666@student.uir.ac.id>
2025-05-11 17:16:50 +0700

    second commit

---
+++ src/components/MyButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <button class="fancy-button" @click="handleClick">
+      {{ buttonText }}
+      <slot />
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      buttonText: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit('customClick')
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .fancy-button {
+    background-color: #42b983;
+    color: white;
+    border: none;
+    padding: 1rem 1.5rem;
+    margin-top: 1rem;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s ease;
+  }
+  
+  .fancy-button:hover {
+    background-color: #2c8f6f;
+  }
+  </style>
